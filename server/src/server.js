cb3b172df4a2f59050969b40c0894a55553d8ed0
Amit Rawat <amitrawat752005@gmail.com>
2026-09-03 12:31:40 +0530

    changes in server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: '../.env.example' }); // using .env.example for now since .env isn't created by default
+require('dotenv').config({ path: '../.env' }); // using .env.example for now since .env isn't created by default
 const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
